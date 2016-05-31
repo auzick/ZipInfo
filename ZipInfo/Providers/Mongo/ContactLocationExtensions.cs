da9165df37912eb8541a6f0eb54a6f121ffbacde
Andy Uzick <andy@uzick.com>
2016-05-31 17:08:54 -0500

    Cleaning up

--- ZipInfo/Providers/Mongo/ContactLocationExtensions.cs
+++ ZipInfo/Providers/Mongo/ContactLocationExtensions.cs
@@ -14,32 +14,7 @@ namespace ZipInfo.Providers.Mongo
             return (ZipCode)ZipInfoManager.Get(zip);
         }
 
-        //public static int GetUtcOffset(this ContactLocation location)
-        //{
-        //    int zip;
-        //    if (!int.TryParse(location.PostalCode, out zip))
-        //        return (int)0;
-        //    return ((ZipCode) ZipInfoManager.Get(zip)).TimezoneOffset;
-        //}
 
-        //public static bool GetParticipatesInDst(this ContactLocation location)
-        //{
-        //    int zip;
-        //    if (!int.TryParse(location.PostalCode, out zip))
-        //        return true;
-        //    return ((ZipCode)ZipInfoManager.Get(zip)).ParticipatesInDst;
-        //}
-
-        //public static DateTime GetLocalDateTime(this ContactLocation location)
-        //{
-        //    int zip;
-        //    if (!int.TryParse(location.PostalCode, out zip))
-        //        return DateTime.MinValue;
-        //    var info = (ZipCode)ZipInfoManager.Get(zip);
-        //    if (info == null)
-        //        return DateTime.MinValue;
-        //    return info.LocalDateTime;
-        //}
 
     }
 }
