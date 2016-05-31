00fb3f12489fb662072dd8d32363b6422b58fb37
Andy Uzick <andy@uzick.com>
2016-05-31 14:56:34 -0500

    Simplifying the ContactLocationExtensions

--- ZipInfo/Providers/Mongo/ContactLocationExtensions.cs
+++ ZipInfo/Providers/Mongo/ContactLocationExtensions.cs
@@ -6,32 +6,40 @@ namespace ZipInfo.Providers.Mongo
 {
     public static class ContactLocationExtensions
     {
-        public static int GetUtcOffset(this ContactLocation location)
+        public static ZipCode GetZipInfo(this ContactLocation location)
         {
             int zip;
             if (!int.TryParse(location.PostalCode, out zip))
-                return (int)0;
-            return ((ZipCode) ZipInfoManager.Get(zip)).TimezoneOffset;
+                return null;
+            return (ZipCode)ZipInfoManager.Get(zip);
         }
 
-        public static bool GetParticipatesInDst(this ContactLocation location)
-        {
-            int zip;
-            if (!int.TryParse(location.PostalCode, out zip))
-                return true;
-            return ((ZipCode)ZipInfoManager.Get(zip)).ParticipatesInDst;
-        }
+        //public static int GetUtcOffset(this ContactLocation location)
+        //{
+        //    int zip;
+        //    if (!int.TryParse(location.PostalCode, out zip))
+        //        return (int)0;
+        //    return ((ZipCode) ZipInfoManager.Get(zip)).TimezoneOffset;
+        //}
 
-        public static DateTime GetLocalDateTime(this ContactLocation location)
-        {
-            int zip;
-            if (!int.TryParse(location.PostalCode, out zip))
-                return DateTime.MinValue;
-            var info = (ZipCode)ZipInfoManager.Get(zip);
-            if (info == null)
-                return DateTime.MinValue;
-            return info.LocalDateTime;
-        }
+        //public static bool GetParticipatesInDst(this ContactLocation location)
+        //{
+        //    int zip;
+        //    if (!int.TryParse(location.PostalCode, out zip))
+        //        return true;
+        //    return ((ZipCode)ZipInfoManager.Get(zip)).ParticipatesInDst;
+        //}
+
+        //public static DateTime GetLocalDateTime(this ContactLocation location)
+        //{
+        //    int zip;
+        //    if (!int.TryParse(location.PostalCode, out zip))
+        //        return DateTime.MinValue;
+        //    var info = (ZipCode)ZipInfoManager.Get(zip);
+        //    if (info == null)
+        //        return DateTime.MinValue;
+        //    return info.LocalDateTime;
+        //}
 
     }
 }
